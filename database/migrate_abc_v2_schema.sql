ea2260fd0c5fd3a009275ba17c2d9e334fb70be9
Jose Felipe Lopez <jose@josefelipelopez.com>
2025-11-11 22:22:33 -0400

    fix: simplificar SQL de migración (sin comentarios) para asegurar parsing correcto

--- database/migrate_abc_v2_schema.sql
+++ database/migrate_abc_v2_schema.sql
@@ -1,12 +1,6 @@
--- Migración: Recrear tabla productos_abc_v2 con schema correcto
-
 DROP TABLE IF EXISTS productos_abc_v2;
-
 DROP TABLE IF EXISTS productos_abc_v2_historico;
-
 DROP TABLE IF EXISTS productos_abc_v2_evolucion;
-
--- 3. Eliminar vistas que dependen de estas tablas
 DROP VIEW IF EXISTS v_abc_v2_resumen;
 DROP VIEW IF EXISTS v_abc_comparacion_velocidad_valor;
 DROP VIEW IF EXISTS v_abc_top_productos;
@@ -15,7 +9,4 @@ DROP VIEW IF EXISTS vista_matriz_abc_xyz;
 DROP VIEW IF EXISTS vista_productos_az_criticos;
 DROP VIEW IF EXISTS vista_productos_bz_cz;
 DROP VIEW IF EXISTS vista_productos_ax_bx;
-DROP VIEW IF EXISTS vista_auditoria_confiabilidad;
-
--- 4. Las tablas se recrearán con schema_abc_v2.sql y schema_abc_xyz.sql
--- Este archivo debe ejecutarse ANTES de esos schemas
+DROP VIEW IF EXISTS vista_auditoria_confiabilidad
